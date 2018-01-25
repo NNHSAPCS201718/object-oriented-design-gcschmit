13ee67f1b3445d3de433d1af0a17d192b0e11327
Geoff Schmit <gcschmit@gmail.com>
2018-01-25 11:33:53 -0600

    added support for change dispensers to CashRegister

--- Class Notes/CashRegisterTester.java
+++ Class Notes/CashRegisterTester.java
@@ -1,23 +1,36 @@
 /**
-   A class to test the CashRegister class.
-*/
+A class to test the CashRegister class.
+ */
 public class CashRegisterTester
 {
-   public static void main(String[] args)
-   {
-      final Coin DOLLAR = new Coin(1.0, "Dollar");
-      final Coin QUARTER = new Coin(0.25, "Quarter");
+    public static void main(String[] args)
+    {
+        final Coin DOLLAR = new Coin(1.0, "Dollar");
+        final Coin QUARTER = new Coin(0.25, "Quarter");
+        final Coin DIME = new Coin(0.10, "Dime");
+        final Coin NICKEL = new Coin(0.05, "Nickel");
+        final Coin PENNY = new Coin(0.01, "Penny");
+        
+        Coin[] availableCoins = { QUARTER, DIME, NICKEL, PENNY };
 
-      CashRegister register = new CashRegister();
+        CashRegister register = new CashRegister();
 
-      register.recordPurchase(1.95);
-      register.recordPurchase(1.40);
-      register.receivePayment(3, DOLLAR);
-      register.receivePayment(2, QUARTER);
+        register.recordPurchase(1.95);
+        register.recordPurchase(1.40);
+        register.receivePayment(3, DOLLAR);
+        register.receivePayment(2, QUARTER);
 
-      double change = register.giveChange();
+        for( Coin coinType : availableCoins )
+        {
+            int coinCount = register.giveChange( coinType );
+            if( coinCount > 0 )
+            {
+                System.out.println( "Dispensed change of " + coinCount + 
+                        " " + coinType.getName());
+            }
+        }
+        
 
-      System.out.println(change);      
-      System.out.println("Expected: 0.15");          
-   }
+        System.out.println("Expected: 0.15");          
+    }
 }
