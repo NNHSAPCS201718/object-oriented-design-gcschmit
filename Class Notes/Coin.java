85713f5741889c06faec31b6b62928ab4a8cdf32
Geoff Schmit <gcschmit@gmail.com>
2018-02-06 11:27:50 -0600

    started to implement the Comparable interface for the Coin class

--- Class Notes/Coin.java
+++ Class Notes/Coin.java
@@ -1,37 +1,66 @@
 /**
-   A coin with a monetary value.
-*/
-public class Coin
+A coin with a monetary value.
+ */
+/*
+ * Use the implements reserved word to specify that this class promises to
+ *  implement all the methods declared in the Comparable interface.
+ */
+public class Coin implements Comparable
 {
-   private double value;
-   private String name;
+    private double value;
+    private String name;
 
-   /**
-      Constructs a coin.
-      @param aValue the monetary value of the coin
-      @param aName the name of the coin
-   */
-   public Coin(double aValue, String aName) 
-   { 
-      value = aValue; 
-      name = aName;
-   }
+    /**
+    Constructs a coin.
+    @param aValue the monetary value of the coin
+    @param aName the name of the coin
+     */
+    public Coin(double aValue, String aName) 
+    { 
+        value = aValue; 
+        name = aName;
+    }
 
-   /**
-      Gets the coin value.
-      @return the value
-   */
-   public double getValue() 
-   {
-      return value;
-   }
+    /**
+    Gets the coin value.
+    @return the value
+     */
+    public double getValue() 
+    {
+        return value;
+    }
 
-   /**
-      Gets the coin name.
-      @return the name
-   */
-   public String getName() 
-   {
-      return name;
-   }
+    /**
+    Gets the coin name.
+    @return the name
+     */
+    public String getName() 
+    {
+        return name;
+    }
+
+    /**
+     * Compares two Coin objects.
+     * 
+     * @param otherObject the object to be compared
+     * @return a negative integer, zero, or a positive integer if this
+     *      coin is less than, equal to, or greater than the specified coin
+     */
+    public int compareTo( Object otherObject )
+    {
+        Coin other = (Coin)otherObject;
+        
+        if( this.value < other.value )
+        {
+            return -1;
+        }
+        else if( this.value > other.value )
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
