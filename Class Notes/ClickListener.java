6f59601232704e50941e7dd9c3cddf3c6e8ad66e
Geoff Schmit <gcschmit@gmail.com>
2018-02-12 09:34:31 -0600

    created first event-driven program

---
+++ Class Notes/ClickListener.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * An action listener that prints a message.
+ *
+ * @author gcschmit
+ * @version 12 February 2018
+ */
+public class ClickListener implements ActionListener
+{
+    public void actionPerformed( ActionEvent event )
+    {
+        System.out.println( "Button " + event.getActionCommand() +
+                " was clicked!" );
+    }
+}
